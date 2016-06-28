b3a0f887fa9a8c7f7a67400154cfd47619af40ad
Guocui Gao <89418.gao@students.itu.edu>
2016-06-28 12:37:00 -0700

    Initial Commit

---
+++ SearchStore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchStore
+//
+//  Created by Shuyan Guo on 6/28/16.
+//  Copyright © 2016 GG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchStore/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SearchStore
+//
+//  Created by Shuyan Guo on 6/28/16.
+//  Copyright © 2016 GG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
